06ff14928f6fa72a48bc130dd1ad58b63cdab59f
JohnGastone <johngastone11@gmail.com>
2024-06-25 23:27:04 +0300

    ++++

--- lib/IntroductionPage.dart
+++ lib/IntroductionPage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,13 +16,33 @@ class _IntroductionPageState extends State<IntroductionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Alama App', style: GoogleFonts.poppins(fontSize: 30)),
-          ],
-        ),
+      backgroundColor: Color.fromRGBO(94, 48, 35, 1),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 250,
+          ),
+          Text('Alama App',
+              style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold)),
+          SizedBox(
+            height: 200,
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              height: 300,
+              width: 200,
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(193, 133, 83, 0.12),
+                  borderRadius:
+                      BorderRadius.only(topRight: Radius.circular(200))),
+            ),
+          )
+        ],
       ),
     );
   }

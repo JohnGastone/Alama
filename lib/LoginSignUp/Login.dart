bc2a7265ceb2f96aa699daf5609b1effff7a9b0a
JohnGastone <johngastone11@gmail.com>
2024-07-01 10:48:35 +0300

    Login Screen ++

--- lib/LoginSignUp/Login.dart
+++ lib/LoginSignUp/Login.dart
@@ -19,9 +19,84 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             children: [
               Text(
-                "Login Page",
+                "Login to your Account",
                 style: GoogleFonts.poppins(
-                    fontSize: 20, fontWeight: FontWeight.bold),
+                    fontSize: 35, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Please signin to your account",
+                style: GoogleFonts.poppins(fontSize: 20, color: Colors.grey),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 150),
+                child: Text("Email address",
+                    style: GoogleFonts.poppins(fontSize: 20)),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "alamaclient@yahoo.com",
+                    labelStyle: GoogleFonts.poppins(
+                      fontSize: 18,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.brown),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 190),
+                child:
+                    Text("Password", style: GoogleFonts.poppins(fontSize: 20)),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    labelText: "***************",
+                    labelStyle: GoogleFonts.poppins(
+                      fontSize: 18,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.brown),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(left: 120),
+                child: Text(
+                  "Forgot Password?",
+                  style: GoogleFonts.poppins(fontSize: 18, color: Colors.brown),
+                ),
               )
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:alamaapp/OnBoardingCarousel/OnboardingCarousel.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Onboardingcarousel(),
     );

280e32e53b8420569d3b067d0027b1dedeb506a0
JohnGastone <johngastone11@gmail.com>
2024-07-03 17:15:27 +0300

    ++++

--- lib/FoodItem/FoodItem.dart
+++ lib/FoodItem/FoodItem.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,6 +15,32 @@ class _FoodItemPageState extends State<FoodItemPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Text(
+                "Food Item",
+                style: GoogleFonts.poppins(
+                    fontSize: 30, fontWeight: FontWeight.w500),
+              ),
+              IconButton(
+                icon: Icon(Icons.share),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
@@ -21,11 +48,26 @@ class _FoodItemPageState extends State<FoodItemPage> {
               SizedBox(
                 height: 30,
               ),
-              Text(
-                "Food Item description will be populated here",
-                style: GoogleFonts.poppins(
-                    fontSize: 30, fontWeight: FontWeight.w500),
-              ),
+              Container(
+                width: 300,
+                height: 250,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("./assets/chipskavu.png"))),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20, right: 7),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        CupertinoIcons.heart_fill,
+                        color: Color(0xFFC18553),
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:alamaapp/FoodItem/FoodItem.dart';
 import 'package:alamaapp/models.dart';
 import 'package:flutter/cupertino.dart';

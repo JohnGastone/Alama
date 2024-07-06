2e89fc4f199cf175d154524352856f4e91c59979
JohnGastone <johngastone11@gmail.com>
2024-07-06 21:36:14 +0300

    Payment method

--- lib/Payment/PaymentMethod.dart
+++ lib/Payment/PaymentMethod.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PaymentMethodPage extends StatefulWidget {
   const PaymentMethodPage({super.key});
@@ -13,9 +14,58 @@ class _PaymentMethodPageState extends State<PaymentMethodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Color(0xFFC18553),
+                child: IconButton(
+                  icon: Center(
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                "Payment Method",
+                style: GoogleFonts.poppins(
+                    fontSize: 30, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            Container(
+              width: 340,
+              height: 80,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Color(0xFFC18553), width: 1)),
+              child: Row(
+                children: [
+                  Text(
+                    "Mobile Money",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
